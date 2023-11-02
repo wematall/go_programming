d099ba3f09c4dae72f3269bf18da007fef90cae6
Andrey <wematall@gmail.com>
2023-11-02 09:45:16 +0300

    max abc funstion using pointers

--- 3.tasks/task_3.1/task_3.1.go
+++ 3.tasks/task_3.1/task_3.1.go
@@ -12,8 +12,23 @@ func max_abc(a, b, c int) int {
 	}
 }
 
+func max_abc_pointers(a, b, c *int) *int {
+
+	if *a >= *b && *a >= *c {
+		return a
+	} else if *b >= *a && *b >= *c {
+		return b
+	} else {
+		return c
+	}
+
+}
 
 func main() {
 	result := max_abc(7, 3, 11)
 	fmt.Printf("Max: %d\n", result)
+
+	var a, b, c int = 7, 3, 11
+	result2 := max_abc_pointers(&a, &b, &c)
+	fmt.Printf("Max: %d\n", *result2)
 }
