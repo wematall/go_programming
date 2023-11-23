4a957dc114b78efdd1c69df23fd9b04477deb914
Andrey <wematall@gmail.com>
2023-11-23 07:26:18 +0300

    func sum recursive

--- 3.tasks/task_3.2/task_3.2.go
+++ 3.tasks/task_3.2/task_3.2.go
@@ -11,10 +11,21 @@ func sum(slice []int) int {
 	return sum
 }
 
+func sum_recurcive(slice []int) int {
+	sum := 0
+	if len(slice) <= 1 {
+		return slice[0]
+	}
+
+	sum = slice[0] + sum_recurcive(slice[1:])
+	return sum
+}
+
 func main() {
 	slc := []int{3, 7, 5}
 
 	sum := sum(slc)
 
 	fmt.Printf("Sum of slice elements: %d\n", sum)
+	fmt.Println(sum_recurcive(slc))
 }
